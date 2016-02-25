e89cf9f2df16b751fff714818c8dadd94034bad1
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-02-25 14:52:54 -0500

    missing semicolon  ¯\_(ツ)_/¯

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ var config = require('./gulp.config')();
 var browserSync = require('browser-sync');
 var superstatic = require('superstatic');
 
-gulp.task('ts-lint', function() {
+gulp.task('ts-lint', function () {
     return gulp.src(config.allTs)
         .pipe(tslint())
         .pipe(tslint.report('prose', {
             emitError: false
         }));
-})
+});
 
 gulp.task('compile-ts', function() {
     var sourceTsFiles = [
@@ -32,22 +32,22 @@ gulp.task('compile-ts', function() {
 });
 
 gulp.task('serve', ['ts-lint', 'compile-ts'], function() {
-    	
+
     gulp.watch([config.allTs], ['ts-lint', 'compile-ts']);
-	
+
     browserSync({
         port: 3000,
         files: ['index.html', '**/*.js'],
         injectChanges: true,
         logFileChanges: false,
-        logLevel: 'silent',    
+        logLevel: 'silent',
         notify: true,
         reloadDelay: 0,
         server: {
             baseDir: ['./'],
             middleware: superstatic({ debug: false})
         }
-    });	
+    });
 });
 
 gulp.task('default', ['serve']);
