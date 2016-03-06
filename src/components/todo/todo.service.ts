0f7775521b84a9b90da6851572a7c2ec833a56de
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-03-05 19:28:39 -0500

    user can create and delete todos from back end

--- src/components/todo/todo.service.ts
+++ src/components/todo/todo.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from 'angular2/core';
 import {Http, Response, Headers} from 'angular2/http';
 // import {Observable} from 'rxjs/Rx';
 import 'rxjs/Rx';
+import {TodoItem} from '../../models';
 
 @Injectable()
 export class TodoService {
@@ -9,7 +10,8 @@ export class TodoService {
 
     getTodos() {
         return this._http.get('http://localhost:3002/api/todo')
-            .map((response: Response) => response.json().data);
+            .map((response: Response) => <TodoItem[]>response.json().data)
+            .do(data => console.log(data));
     }
 
     newTodo(todo) {
@@ -18,11 +20,17 @@ export class TodoService {
         return this._http.post('http://localhost:3002/api/todo',
             todo,
             { headers: headers })
-                .map((response: Response) => response.json())
-                .subscribe(
-                    data => console.log('data: ', data),
-                    err => console.log('err: ', err),
-                    () => console.log('newTodo Posted')
-                );
+            .map((response: Response) => response.json())
+            .subscribe(
+                data => console.log('data: ', data),
+                err => console.log('err: ', err),
+                () => console.log('newTodo Posted')
+            );
+    }
+
+    deleteTodo(id) {
+        console.log('back end deleting...');
+        return this._http.delete('http://localhost:3002/api/todo/' + id)
+            .subscribe(response => console.log(response));
     }
 }
