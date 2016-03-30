3353e6d76a74ae6b8b70b3975d3ded617ff61b2a
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-03-29 20:23:49 -0400

    add test for id param

--- app/components/about/about.component.ts
+++ app/components/about/about.component.ts
@@ -1,46 +1,21 @@
-// import {Component, View} from 'angular2/core';
-// import {RouteParams} from 'angular2/router';
-
-// @Component({
-//     selector: 'about'
-// })
-// @View({
-//     template: `
-// 		Welcome to the about page! This is the ID: {{id}}
-// 	`
-// })
-// export class About {
-//     id: string;
-//     constructor(params: RouteParams) {
-//         this.id = params.get('id');
-//     }
-
-//     ngOnInit () {
-//         console.log('hello `About` component');
-//     }
-// }
-
-import {Component} from 'angular2/core';
-
-/*
- * We're loading this component asynchronously
- * We are using some magic with es6-promise-loader that will wrap the module with a Promise
- * see https://github.com/gdi2290/es6-promise-loader for more info
- */
-
-console.log('`About` component loaded asynchronously');
+import {Component, View} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
 
 @Component({
-  selector: 'about',
-  template: `patrick@AngularClass.com`
+    selector: 'about'
+})
+@View({
+    template: `
+		Welcome to the about page! This is the ID: {{id}}
+	`
 })
 export class About {
-  constructor() {
+    id: string;
+    constructor(params: RouteParams) {
+        this.id = params.get('id');
+    }
 
-  }
-
-  ngOnInit() {
-    console.log('hello `About` component');
-  }
-
+    ngOnInit () {
+        console.log('hello `About` component');
+    }
 }
--- app/components/about/about.spec.ts
+++ app/components/about/about.spec.ts
@@ -1,29 +1,35 @@
 import {
-  it,
-  inject,
-  injectAsync,
-  describe,
-  beforeEachProviders,
-  TestComponentBuilder
+    it,
+    inject,
+    injectAsync,
+    describe,
+    beforeEachProviders,
+    TestComponentBuilder
 } from 'angular2/testing';
+import {RouteParams} from 'angular2/router';
 
-import {Component} from 'angular2/core';
+import {Component, provide} from 'angular2/core';
 
 // Load the implementations that should be tested
 import {About} from './about.component';
 
 describe('About', () => {
-  // provide our implementations or mocks to the dependency injector
-  beforeEachProviders(() => [
-    About
-  ]);
+    // provide our implementations or mocks to the dependency injector
+    beforeEachProviders(() => [
+        About,
+        provide(RouteParams, { useValue: new RouteParams({ id: 'hello, world!' }) }),
+    ]);
 
-  it('should log ngOnInit', inject([ About ], (about) => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
+    it('should log ngOnInit', inject([About], (about) => {
+        spyOn(console, 'log');
+        expect(console.log).not.toHaveBeenCalled();
 
-    about.ngOnInit();
-    expect(console.log).toHaveBeenCalled();
-  }));
+        about.ngOnInit();
+        expect(console.log).toHaveBeenCalled();
+    }));
+
+    it('the id should match the param', inject([About], (about) => {
+        expect(about.id).toEqual('hello, world!');
+    }));
 
 });
