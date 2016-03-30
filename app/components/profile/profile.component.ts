7e1045f4123e11dba68de44da943269ab5657260
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-03-29 20:33:55 -0400

    reindent file

--- app/components/profile/profile.component.ts
+++ app/components/profile/profile.component.ts
@@ -4,46 +4,46 @@ import {tokenNotExpired} from 'angular2-jwt';
 import {AuthHttp} from 'angular2-jwt';
 
 @Component({
-	selector: 'profile'
+    selector: 'profile'
 })
 @View({
-	template: `
-	 <img src="{{profile.picture}}" style="width: 50px" /> {{profile.name}}
-   <h2>Chuck quote of the day</h2>
-   {{quote}}
+    template: `
+        <img src="{{profile.picture}}" style="width: 50px" /> {{profile.name}}
+        <h2>Chuck quote of the day</h2>
+        {{quote}}
 	`
 })
 @CanActivate(() => tokenNotExpired())
 export class Profile {
-  profile: any;
-  quote: any;
+    profile: any;
+    quote: any;
 
-  constructor(public authHttp: AuthHttp) {
-    this.profile = JSON.parse(localStorage.getItem('profile'));
-    this.getSecretThing();
-    this.testApi();
-  }
+    constructor(public authHttp: AuthHttp) {
+        this.profile = JSON.parse(localStorage.getItem('profile'));
+        this.getSecretThing();
+        this.testApi();
+    }
 
-  getSecretThing() {
-    this.authHttp.get('http://localhost:3002/api/quote')
-      .subscribe(
-        data => {
-          console.log(data.json());
-        //   this.quote = data.json();
-        },
-        err => console.log(err),
-        () => console.log('Complete')
-      );
-  }
-  testApi() {
-    this.authHttp.get('http://localhost:3002/api/todo')
-      .subscribe(
-        data => {
-          console.log(data.json().data);
-          this.quote = data.json().info;
-        },
-        err => console.log(err),
-        () => console.log('Todo!')
-      );
-  }
+    getSecretThing() {
+        this.authHttp.get('http://localhost:3002/api/quote')
+            .subscribe(
+            data => {
+                console.log(data.json());
+                //   this.quote = data.json();
+            },
+            err => console.log(err),
+            () => console.log('Complete')
+            );
+    }
+    testApi() {
+        this.authHttp.get('http://localhost:3002/api/todo')
+            .subscribe(
+            data => {
+                console.log(data.json().data);
+                this.quote = data.json().info;
+            },
+            err => console.log(err),
+            () => console.log('Todo!')
+            );
+    }
 }
