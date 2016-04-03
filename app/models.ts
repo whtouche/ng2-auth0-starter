76e1a827bd0c1a6f4b23ac6cd478f0fbca852040
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-04-03 18:50:05 -0400

    formatting

--- app/models.ts
+++ app/models.ts
@@ -1,4 +1,7 @@
 export class TodoItem {
-  constructor(public text: string, public completed: boolean, public _id: any) {
+  constructor(
+      public text: string,
+      public completed: boolean,
+      public _id: any) {
   }
 }
